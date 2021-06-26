baa9aceac93e01fda5c32659c81ae37af14296b0
Otutu qudus abolore <qudus_kudos@yahoo.com>
2021-06-26 11:22:41 +0100

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Header(){
     return(
         <header >
-          <img src="./Trollface.png" alt="pix" style={{height:130}}/>
-          <h3><p>Meme Generator gg</p></h3>
+          <img src= "Trollface.png" alt="pix" style={{height:130}}/>
+          <h3><p>Meme Generator </p></h3>
         </header>
     )
 }
